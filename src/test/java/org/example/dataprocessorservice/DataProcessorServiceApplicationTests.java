1edda732460f9a4bbf902a32fa0e52998e7203da
Zinovev Ivan <izinovev77@gmail.com>
2025-06-10 16:02:35 +0300

    init commit

---
+++ src/test/java/org/example/dataprocessorservice/DataProcessorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dataprocessorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataProcessorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
