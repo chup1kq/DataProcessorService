7d3bdc5aecdaee494e8da6dc8913f9ba355e0486
Zinovev Ivan <izinovev77@gmail.com>
2025-06-16 12:05:24 +0300

    add GET /page endpoint for request logs

--- src/main/java/org/example/dataprocessorservice/controller/RequestController.java
+++ src/main/java/org/example/dataprocessorservice/controller/RequestController.java
@@ -1,9 +1,12 @@
 package org.example.dataprocessorservice.controller;
 
 import lombok.AllArgsConstructor;
+import org.example.dataprocessorservice.entity.RequestLog;
 import org.example.dataprocessorservice.service.ConversionService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
@@ -15,5 +18,26 @@ public class RequestController {
     @PostMapping(path = "/request",consumes = MediaType.APPLICATION_XML_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public String convertXmlToJson(@RequestBody String xml) {
         return conversionService.convertXmlToJson(xml);
+    }
+
+    @GetMapping(path = "/page", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<RequestLog> getRequestData(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(required = false) Long minProcessingTime,
+            @RequestParam(required = false) Long maxProcessingTime,
+            @RequestParam(required = false) Integer minXmlTags,
+            @RequestParam(required = false) Integer maxXmlTags,
+            @RequestParam(required = false) Integer minJsonKeys,
+            @RequestParam(required = false) Integer maxJsonKeys
+    ) {
+        return conversionService.getRequestLogs(
+                page,
+                minProcessingTime,
+                maxProcessingTime,
+                minXmlTags,
+                maxXmlTags,
+                minJsonKeys,
+                maxJsonKeys
+        );
     }
 }
