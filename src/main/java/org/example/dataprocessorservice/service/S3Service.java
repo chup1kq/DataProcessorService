e1f972c184c0dca54d4d6de43ef6412ebc3c45bb
Zinovev Ivan <izinovev77@gmail.com>
2025-06-20 12:39:26 +0300

    feat: add S3Service

---
+++ src/main/java/org/example/dataprocessorservice/service/S3Service.java
@@ -0,0 +1,58 @@
+package org.example.dataprocessorservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.dataprocessorservice.exception.S3DownloadException;
+import org.example.dataprocessorservice.exception.S3UploadException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    public String uploadJson(String json) {
+        try {
+            String key = UUID.randomUUID().toString();
+
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .contentType("application/json")
+                    .build();
+
+            s3Client.putObject(request, RequestBody.fromString(json));
+
+            return key;
+        } catch (S3Exception e) {
+            throw new S3UploadException();
+        }
+    }
+
+    public String downloadJson(String key) {
+        try {
+            GetObjectRequest request = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+
+            ResponseBytes<GetObjectResponse> responseBytes = s3Client.getObjectAsBytes(request);
+            return responseBytes.asUtf8String();
+        } catch (S3Exception e) {
+            throw new S3DownloadException();
+        }
+    }
+}
