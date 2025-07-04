9f453ffe8d570098226bd3eabedf4ba36fdf7d4c
Иван Зиновьев <izinovev77@gmail.com>
2025-07-04 11:14:47 +0300

    Revert "RequestLogRepository"
    This reverts commit f286ad6269bd4ddbdadf6140e231f37e69c0b7c3.

--- src/main/java/org/example/dataprocessorservice/repository/RequestLogRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.dataprocessorservice.repository;
-
-import org.example.dataprocessorservice.entity.RequestLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface RequestLogRepository extends JpaRepository<RequestLog, Long>, JpaSpecificationExecutor<RequestLog> {
-}
