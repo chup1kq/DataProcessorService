f868790da912574f0f86bc93d8a4ce1bb8584ca2
Иван Зиновьев <izinovev77@gmail.com>
2025-07-04 11:14:47 +0300

    Revert "refactor: extract RequestLogFilterDto for readability"
    This reverts commit 818ec2cf30dbb5476d80f83bb794c635e6faec20.

--- src/main/java/org/example/dataprocessorservice/controller/RequestController.java
+++ src/main/java/org/example/dataprocessorservice/controller/RequestController.java
@@ -30,15 +30,16 @@ public class RequestController {
             @RequestParam(required = false) Integer minJsonKeys,
             @RequestParam(required = false) Integer maxJsonKeys
     ) {
-        RequestLogFilterDto requestLogFilterDto = RequestLogFilterDto.builder()
-                .minProcessingTime(minProcessingTime)
-                .maxProcessingTime(maxProcessingTime)
-                .minXmlTags(minXmlTags)
-                .maxXmlTags(maxXmlTags)
-                .minJsonKeys(minJsonKeys)
-                .maxJsonKeys(maxJsonKeys)
-                .build();
-
-        return conversionService.getRequestLogs(requestLogFilterDto, page);
+        return conversionService.getRequestLogs(
+                RequestLogFilterDto.builder()
+                        .minProcessingTime(minProcessingTime)
+                        .maxProcessingTime(maxProcessingTime)
+                        .minXmlTags(minXmlTags)
+                        .maxXmlTags(maxXmlTags)
+                        .minJsonKeys(minJsonKeys)
+                        .maxJsonKeys(maxJsonKeys)
+                        .build(),
+                page
+        );
     }
 }
