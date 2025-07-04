81859665e46eab922f766234ff1d74034e9c82a2
Иван Зиновьев <izinovev77@gmail.com>
2025-07-04 11:14:47 +0300

    Revert "feat: Add findByExternalIdIsNull repository method"
    This reverts commit 18509d5fa678a35afee25abc766b983e6219e12b.

--- src/main/java/org/example/dataprocessorservice/repository/RequestLogRepository.java
+++ src/main/java/org/example/dataprocessorservice/repository/RequestLogRepository.java
@@ -8,11 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
-
 public interface RequestLogRepository extends JpaRepository<RequestLog, Long>, JpaSpecificationExecutor<RequestLog> {
-
-    List<RequestLog> findByExternalIdIsNull();
 
     default Page<RequestLog> getRequestLogs(RequestLogFilterDto requestLogFilterDto, Pageable pageable) {
         return findAll(RequestLogSpecifications.withFilters(requestLogFilterDto), pageable);
