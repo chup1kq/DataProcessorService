715782ce0fa0815a342d163653dcc8fd66cfc037
Иван Зиновьев <izinovev77@gmail.com>
2025-07-04 11:14:47 +0300

    Revert "chore: setup Liquibase, application.yml, Dockerfile and docker-compose"
    This reverts commit 4652d8bf36a24f36eea62a2315cd40dc19d99c91.

--- src/main/resources/db/changelog/changeset/0000__init_tables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS request_log (
-    id SERIAL PRIMARY KEY,
-    json_payload TEXT,
-    processing_time_ms BIGINT NOT NULL,
-    xml_tag_count INTEGER,
-    json_key_count INTEGER
-);
