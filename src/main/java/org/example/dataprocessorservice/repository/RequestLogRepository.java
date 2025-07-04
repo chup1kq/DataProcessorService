41687ae2460c5eef3910cb872c597c5665c82f6a
Иван Зиновьев <izinovev77@gmail.com>
2025-07-04 11:14:47 +0300

    Revert "Added receiving requests with parameters"
    This reverts commit 8161988ef39f0d2afd4986d4cfdafbbe5cccc683.

--- src/main/java/org/example/dataprocessorservice/repository/RequestLogRepository.java
+++ src/main/java/org/example/dataprocessorservice/repository/RequestLogRepository.java
@@ -1,34 +1,8 @@
 package org.example.dataprocessorservice.repository;
 
 import org.example.dataprocessorservice.entity.RequestLog;
-import org.example.dataprocessorservice.specifications.RequestLogSpecifications;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
-
 public interface RequestLogRepository extends JpaRepository<RequestLog, Long>, JpaSpecificationExecutor<RequestLog> {
-
-    default List<RequestLog> getRequestLogs(
-            int page,
-            int pageSize,
-            Long minProcessingTime,
-            Long maxProcessingTime,
-            Integer minXmlTags,
-            Integer maxXmlTags,
-            Integer minJsonKeys,
-            Integer maxJsonKeys
-    ) {
-        Pageable pageable = PageRequest.of(page, pageSize);
-        return findAll(RequestLogSpecifications.withFilters(
-                minProcessingTime,
-                maxProcessingTime,
-                minXmlTags,
-                maxXmlTags,
-                minJsonKeys,
-                maxJsonKeys
-        ), pageable).toList();
-    }
 }
